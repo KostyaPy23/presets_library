3e67068656d02b0db971efd1163e08ce5200028f
Kostya <negrunKostya@gmail.com>
2016-07-04 00:20:50 +0300

    add images to library

--- js/scripts.js
+++ js/scripts.js
@@ -26,21 +26,14 @@ $(document).ready(function() {
       }
 	});
 
-	// Show code from repositiry
-	$(".btn").click(function() {
-		$(".code").toggle(300);
-	});
-
 	// Tabs
 	$(function() {
-
 	 	$('ul.tabs-caption').on('click', 'li:not(.active)', function() {
 	    	$(this)
 	      	.addClass('active').siblings().removeClass('active')
 	      	.closest('div.tabs').find('div.tabs-content').removeClass('active').eq($(this).index()).addClass('active');
 	  	});
 	});
-
 
 });
 
